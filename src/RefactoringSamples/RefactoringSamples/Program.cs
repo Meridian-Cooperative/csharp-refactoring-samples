bae18d44fcc6565d7f6003e9b602c84ffc25cda9
phani vanka <phaneendrav@meridian.coop>
2023-03-27 12:32:51 -0400

    added Person class and corresponding tests

---
+++ src/RefactoringSamples/RefactoringSamples/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RefactoringSamples;
+
+
+List<Person> people = new List<Person>();
+
+people.Add(new Person()
+{
+    Name = "John Silver",
+    DateOfBirth = new DateTime(1992, 1, 1)
+});
+
+people.Add(new Person()
+{
+    Name = "Jack Sparrow",
+    DateOfBirth = new DateTime(1899, 1, 1)
+});
+
+people.Add(new Person()
+{
+    Name = "Baby Groot",
+    DateOfBirth = new DateTime(2005, 8, 1)
+});
+
+Console.WriteLine($"Of total population of {people.Count}, {people.Count(p => p.CanVote())} can vote");
