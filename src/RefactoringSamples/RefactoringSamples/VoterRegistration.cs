c54bc79c9f0ad62a253a6e7853c422bf6a2d9def
phani vanka <phaneendrav@meridian.coop>
2023-03-27 15:30:34 -0400

    added VoterRegistrationService and VoterRegistryRepository classes

---
+++ src/RefactoringSamples/RefactoringSamples/VoterRegistration.cs
@@ -0,0 +1,27 @@
+﻿namespace RefactoringSamples
+{
+    public class VoterRegistrationService
+    {
+        // make this class testable
+        public bool RegisterForVote(Person person)
+        {
+            if (person.CanVote())
+            {
+                VoterRegistryRepository.AddToRegistry(person);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    // cannot change this class as its used widely in system
+    public static class VoterRegistryRepository
+    {
+        static List<Person> _listOfVoters = new List<Person>();
+
+        public static void AddToRegistry(Person p)
+        {
+            _listOfVoters.Add(p);
+        }
+    }
+}
