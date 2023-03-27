627bdcb288d721ebeba69d87807c2839480fab88
phani-vanka <128707664+phani-vanka@users.noreply.github.com>
2023-03-27 17:00:39 -0400

    Second sample
    * added VoterRegistrationService, VoterRegistryRepository classes and tests for VoterRegistrationService

---
+++ src/RefactoringSamples/RefactoringSamples/VoterRegistration.cs
@@ -0,0 +1,35 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace RefactoringSamples
+{
+    // make this class testable
+    public class VoterRegistrationService
+    {
+        public bool RegisterForVote(Person person)
+        {
+            if (person.CanVote())
+            {
+                VoterRegistryRepository.AddToRegistry(person);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    // cannot change this class as its used widely in system
+    public static class VoterRegistryRepository
+    {
+        static readonly String _connectionString = "DATA SOURCE=10.1.1.1:1521/voterdb;User Id=user1;Password=pwd1;Pooling=True;Connection Timeout=1";
+        public static void AddToRegistry(Person p)
+        {
+            using (OracleConnection conn = new OracleConnection(_connectionString))
+            {
+                conn.Open();
+
+                OracleCommand insertCommand = conn.CreateCommand();
+                insertCommand.CommandText = $"insert into voters values ('{p.Name}')";
+                insertCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
